e2653555826f37c6efb1042ff09d1fb1b4e8bf00
Aiton C <aitoncsubscriptions@gmail.com>
2021-03-26 15:50:45 +0200

    Initial Commit

--- Hoop.py
+++ Hoop.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Hoop.ui'
+# Form implementation generated from reading ui file '.\interface\Hoop.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
